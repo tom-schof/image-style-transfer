9473ce7cee44157dd57318202b6238a6329c91ca
Tom Schof <tom-schof@users.noreply.github.com>
2018-10-30 11:08:18 -0400

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useMongoClient: true
+  }
+);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
