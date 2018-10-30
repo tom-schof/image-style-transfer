6f6ff00eb775636153f3d0da0bdbdbdfce37555d
rmaharja@gmail.com <rmaharja@gmail.com>
2018-10-30 13:24:04 -0400

    fixed dependencies

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+const PORT = process.env.PORT || 3001;
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mern",
   {
-    useMongoClient: true
+    useNewUrlParser: true
   }
 );
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
