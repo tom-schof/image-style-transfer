4fc6828444a8234d5313c813e51832617f05fbbd
Tom Schof <tom-schof@users.noreply.github.com>
2018-10-31 11:59:08 -0400

    routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Routes from "./Routes";
 
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
