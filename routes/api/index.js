923d4dc4f89a3c2438ffde95f0469686fb731623
Brandon Shoun-Corry <40499862+BrandonSCorry@users.noreply.github.com>
2018-10-31 19:38:38 -0400

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const uploadRoutes = require("./uploads");
 
-// Book routes
+// uploaded img routes
 router.use("/uploads", uploadRoutes);
 
 module.exports = router;
