39362385e025cde879f85e0b64dae50b2d9db66d
Tom Schof <tom-schof@users.noreply.github.com>
2018-10-31 11:56:52 -0400

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Nav from "./components/Nav";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Saved from "./pages/Saved";
-import Community from "./pages/Community";
-import NoMatch from "./pages/NoMatch";
-import './App.css';
-import {
-  ReactiveBase,
-  CategorySearch,
-  RatingsFilter,
-  ResultCard
-} from '@appbaseio/reactivesearch';
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+// import Nav from "./components/Nav";
+// import Login from "./pages/Login";
+// import Home from "./pages/Home";
+// import Saved from "./pages/Saved";
+// import Community from "./pages/Community";
+// import NoMatch from "./pages/NoMatch";
+// import './App.css';
+// import {
+//   ReactiveBase,
+//   CategorySearch,
+//   RatingsFilter,
+//   ResultCard
+// } from '@appbaseio/reactivesearch';
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/saved" component={Saved} />
-        <Route exact path="/community" component={Community} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+// const App = () => (
+//   <Router>
+//     <div>
+//       <Nav />
+//       <Switch>
+//         <Route exact path="/" component={Login} />
+//         <Route exact path="/home" component={Home} />
+//         <Route exact path="/saved" component={Saved} />
+//         <Route exact path="/community" component={Community} />
+//         <Route component={NoMatch} />
+//       </Switch>
+//     </div>
+//   </Router>
+// );
 
 
-  export default App;
+//   export default App;
