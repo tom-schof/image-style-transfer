ee598e75418189e50e3a3e9ac6675f2e0b4d65fe
rmaharja@gmail.com <rmaharja@gmail.com>
2018-11-01 12:30:15 -0400

    connecting react to server for heroku

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 //assigning PORT;
 const PORT = process.env.PORT || 3001; 
 
+//database connection:
 
-app.get("/", (req,res) => {
-  res.send("Hello...");
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/styletransferdb",
+  {
+    useNewUrlParser: true
+  }
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 app.listen(PORT);
