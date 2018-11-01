2bd2b2869c2b4f9bcd7d23f37b1f3324945be3fe
rmaharja@gmail.com <rmaharja@gmail.com>
2018-11-01 14:53:29 -0400

    mongo working

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Nav from "./components/Nav";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import Community from "./pages/Community";
+import NoMatch from "./pages/NoMatch";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          Gotta Catchem all
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/saved" component={Saved} />
+        <Route exact path="/community" component={Community} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-export default App;
+
+  export default App;
