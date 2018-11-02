1690ecd04bc256b9ac89b9201ed3b11a72b0aeb1
Tom Dennis <tdennis913@gmail.com>
2018-11-02 11:29:21 -0400

    merged src

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Jumbotron.css";
 
-const Jumbotron = ({ children }) => (
-  <div
-    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-    className="jumbotron"
-  >
-    {children}
+const Jumbotron = () => (
+    <div className="jumbotron shadow">
+    <div className="container text-center">
+      <h1 className="display-4">Image Creator</h1>
+      <p className="lead">Apply one of our style models to an image of your choice and produce art using machine learning!</p>
+    </div>
   </div>
 );
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-nav">
     <a className="navbar-brand" href="/">
-      EMNIST Data Training
+      neurostyle
     </a>
   </nav>
 );
